2ecde6c4b4b2d48cb4c3d75d67d515d521cb4489
Koen Samyn <samyn.koen@gmail.com>
2022-05-04 16:57:28 +0200

    Restructuring project into lib project and main project to make unit tests possible.

---
+++ VecMathREPLCpp/unittests/UnitTestMain.cpp
@@ -0,0 +1,23 @@
+
+
+#include "rapidjson.h"
+#include "document.h"
+#include "stream.h"
+#include "filereadstream.h"
+#include "EMath.h"
+#include "RayTracerFunctions.h"
+#include "RayTracerFunctionsW2.h"
+#include "RayTracerFunctionsW3.h"
+#include "RayTracerFunctionsW4.h"
+#include "RayTracerFunctionsW5.h"
+#include "TestUtil.h"
+#include "Week1Test.h"
+#include "Week2Test.h"
+#include "Week3Test.h"
+// #include "Week4Test.h"
+// #include "Week5Test.h"
+
+
+
+
+
