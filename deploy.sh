896324e35b46f98b67e8f8c1c517cd53a40614ac
Arne Six <six.arne@gmail.com>
2022-05-04 20:07:19 +0200

    Fixed website

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # abort on errors
-#set -e
+set -e
 
 # build
 npm run docs:build
@@ -11,6 +11,10 @@ cd docs/.vitepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
