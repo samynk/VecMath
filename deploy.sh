2ad45566876d5122d14a58e12eca4667cd13128a
Koen Samyn <samyn.koen@gmail.com>
2022-05-14 19:02:24 +0200

    trying to install the documentation

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:SixArne/VecMath.git main:gh-pages
+git push -f git@github.com:samynk/VecMath.git main:gh-pages
 
 cd -
 
