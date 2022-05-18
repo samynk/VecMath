cf94787b056449b7e237878ffe9e6d53dd8eff25
Koen Samyn <samyn.koen@gmail.com>
2022-05-18 14:35:06 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:samynk/VecMath.git main:gh-pages
+git push origin main:gh-pages
 
 cd -
 
