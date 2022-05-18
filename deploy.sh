904e65535ae9fb5bffbffbd2eaaca74126aa35c2
Koen Samyn <samyn.koen@gmail.com>
2022-05-18 09:22:56 +0200

    Added .js file for vitepress

--- deploy.sh
+++ deploy.sh
@@ -12,10 +12,9 @@ cd docs/.vitepress/dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
+git init -b main
 git add -A
 git commit -m 'deploy'
-
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
