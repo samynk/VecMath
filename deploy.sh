d80c125a6ea06a5b6fc75d6bd3cca1c697fbb9b7
Koen Samyn <samyn.koen@gmail.com>
2022-05-20 08:57:06 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,12 +16,6 @@ cd docs/.vitepress/dist
 git init -b main
 git add -A
 git commit -m 'deploy'
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push origin main:gh-pages
-
 cd -
 
 $SHELL
