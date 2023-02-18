bc5fe7bd5428d62fb9bf2be129dfdebc115ecc9a
johnythecarrot <tuurmartens4@gmail.com>
2023-02-18 17:21:19 +0100

    feat: accessing elements of vectors

--- VecMathREPLRust/src/evaluation/functions.rs
+++ VecMathREPLRust/src/evaluation/functions.rs
@@ -50,6 +50,10 @@ type CallResult = Result<Expression, Box<ParseError>>;
 pub fn evaluate_call(fn_name: Spanned<String>, arguments: Arguments) -> CallResult {
     match fn_name.content.to_lowercase().as_str() {
         "sqrt" => sqrt(arguments),
+        "element" => element(arguments),
+        "x" => get_element_sugar(arguments, 0),
+        "y" => get_element_sugar(arguments, 1),
+        "z" => get_element_sugar(arguments, 2),
         "nthroot" => nth_root(arguments),
         _ => Err(Box::new(ParseError::custom(
             fn_name.span,
@@ -79,6 +83,78 @@ fn sqrt(args: Arguments) -> CallResult {
         Ok(Expression::Scalar(result))
     } else {
         incorrect_num_args!(arguments_span, 1, argument_expressions.len())
+    }
+}
+
+fn get_element_sugar(args: Arguments, index: usize) -> CallResult {
+    let argument_expressions = args.content;
+    let arguments_span = args.span;
+    let required_args = 1;
+
+    if argument_expressions.len() == required_args {
+        if let Expression::Vec(vec) = &argument_expressions[0].content {
+            let element = vec.get(index).ok_or(Box::new(Simple::custom(
+                arguments_span,
+                format!("No element at index {}", index + 1),
+            )))?;
+
+            Ok(element.content.clone())
+        } else {
+            invalid_arg_types!(
+                arguments_span,
+                vec![Expression::Vec(vec![])],
+                vec![argument_expressions[0].content.clone()]
+            )
+        }
+    } else {
+        incorrect_num_args!(arguments_span, required_args, argument_expressions.len())
+    }
+}
+
+fn element(args: Arguments) -> CallResult {
+    let argument_expressions = args.content;
+    let arguments_span = args.span;
+    let required_args = 2;
+
+    if argument_expressions.len() == required_args {
+        if let (Expression::Vec(vec), Expression::Scalar(index)) = (
+            &argument_expressions[0].content,
+            &argument_expressions[1].content,
+        ) {
+            if index <= &0f64 {
+                return Err(Box::new(Simple::custom(
+                    argument_expressions[1].span.clone(),
+                    "Element index must be at least 1",
+                )));
+            }
+
+            if index.fract() > 0f64 {
+                return Err(Box::new(Simple::custom(
+                    argument_expressions[1].span.clone(),
+                    "Index must be an integer",
+                )));
+            }
+
+            let vec_index = *index as usize;
+
+            let element = vec.get(vec_index - 1).ok_or(Box::new(Simple::custom(
+                arguments_span,
+                format!("No element at index {}", vec_index),
+            )))?;
+
+            Ok(element.content.clone())
+        } else {
+            invalid_arg_types!(
+                arguments_span,
+                vec![Expression::Vec(vec![]), Expression::Scalar(f64::default())],
+                vec![
+                    argument_expressions[0].content.clone(),
+                    argument_expressions[1].content.clone()
+                ]
+            )
+        }
+    } else {
+        incorrect_num_args!(arguments_span, required_args, argument_expressions.len())
     }
 }
 
